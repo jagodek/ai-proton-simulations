7184c12e14bc84e546ec0ee97089328d5a54d477
Michał Godek <godek.main@gmail.com>
2026-05-07 19:47:15 +0200

    fix find failed simulations

--- utils/find_failed_simulations.py
+++ utils/find_failed_simulations.py
@@ -20,7 +20,7 @@ files = [f for f in files if f.startswith("_")]
 for file in sorted(files, key=lambda f: int(f.split("_")[1])):
     # print(Path(SAVE_DATA_LOCATION, f"batch{batch_num}","output", "z_profile.bdo"))
     output_files = os.listdir(Path(SAVE_DATA_LOCATION, f"batch{batch_num}", file, "output"))
-    output_files = [f for f in output_files if f.endswith(".bdo") or f.endswith(".log")]
+    output_files = [f for f in output_files if not f.endswith(".bdo") and not f.endswith(".log")]
     for output_file in output_files:
         if "for" in output_file:
             print(f"Failed simulation {file}")
