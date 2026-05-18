b25a5f85a5eb0128d8a3056a0e783e2aaa26b724
Michał Godek <godek.main@gmail.com>
2026-05-18 17:54:23 +0200

    updated memory hyperqueue

--- gen4/run_shieldhit.sh
+++ gen4/run_shieldhit.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 LINE_NUM=$(($SLURM_ARRAY_TASK_ID+1))
-
 line=$(sed -n "${LINE_NUM}p" commands_batch24)
 
-
 echo $line
-
-
 eval $line
 
 # sbatch --array=0-24 --mem-per-cpu=1G run_shieldhit.sh
