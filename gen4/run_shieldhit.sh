0368c29da4ad873dc7efda0130e6c4e5f6272e0e
Michał Godek <godek.main@gmail.com>
2026-05-18 17:34:33 +0200

    hyperqueue scritps gen4

--- gen4/run_shieldhit.sh
+++ gen4/run_shieldhit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-
-# ROOT_DIR=/home/michal/slrm/gen4/batch23
-
 LINE_NUM=$(($SLURM_ARRAY_TASK_ID+1))
 
 line=$(sed -n "${LINE_NUM}p" commands_batch24)
@@ -11,9 +8,6 @@ line=$(sed -n "${LINE_NUM}p" commands_batch24)
 echo $line
 
 
-# cd $ROOT_DIR/_$SLURM_ARRAY_TASK_ID
-# pwd
-#echo $line > new_file
 eval $line
 
 # sbatch --array=0-24 --mem-per-cpu=1G run_shieldhit.sh
