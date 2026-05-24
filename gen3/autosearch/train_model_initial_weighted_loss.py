58674bf0b3eb7cdd6ac85c922bd4fb4c9fe7c749
Michał Godek <godek.main@gmail.com>
2026-05-24 12:38:29 +0200

    weighted loss

--- gen3/autosearch/train_model_initial_weighted_loss.py
+++ gen3/autosearch/train_model_initial_weighted_loss.py
@@ -14,6 +14,7 @@ HOME = "/home/michal/slrm/gen3/autosearch"
 if os.getenv("PLG_GROUPS_STORAGE"):
     HOME = "/net/people/plgrid/plgmichalgodek/workspace/ai-proton-simulations/gen3/autosearch"
 LOGS_PATH = Path(HOME, "tmp", "logs") 
+TMP_DIR = Path(HOME, "tmp"+slurm_job_id)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Device: {device}")
@@ -55,19 +56,30 @@ normalized_data_dlet_protons_test = data_dlet_protons_test/max_dlet_protons
 
 
 
-reshaped_data_dose = data_dose.reshape(seeds_per_energy, -1, 400)
-averaged_data_dose = reshaped_data_dose.mean(axis=1)
 
 reshaped_data_fluence = data_fluence_protons.reshape(-1,seeds_per_energy,  400)
 averaged_data_fluence = reshaped_data_fluence.mean(axis=1)
-weights_mask = np.ones((len(averaged_data_fluence),400))
+weights_mask_train = np.ones((len(averaged_data_fluence),400))
 
 for i in range(len(averaged_data_fluence)):
     max_fluence_in_i = np.max(averaged_data_fluence[i])
     for k in range(400):
         if averaged_data_fluence[i][k] < 0.001*max_fluence_in_i:
-            weights_mask[i][:k] = 100
+            weights_mask_train[i][:k] = 100
             break
+weights_mask_train = weights_mask_train / weights_mask_train.mean(axis=1, keepdims=True)
+
+
+
+weights_mask_test = np.ones((len(data_fluence_protons_test),400))
+
+for i in range(len(weights_mask_test)):
+    max_fluence_in_i = np.max(data_fluence_protons_test[i])
+    for k in range(400):
+        if data_fluence_protons_test[i][k] < 0.001*max_fluence_in_i:
+            weights_mask_test[i][:k] = 100
+            break
+weights_mask_test = weights_mask_test / weights_mask_test.mean(axis=1,keepdims=True)
 
 
 
@@ -75,7 +87,7 @@ for i in range(len(averaged_data_fluence)):
 with open(LOGS_PATH,"w+") as f:
     pass
 
-def test_model(model, criterion, device):
+def test_model(model, criterion):
     """
     Evaluates the trained model on the test dataset.
     """
@@ -118,10 +130,20 @@ def test_model(model, criterion, device):
 
         predictions_halves = model(X_test_halves_tensor)
         test_loss_halves = criterion(predictions_halves, Y_test_halves).item()
+
+
+
+    
+
     criterion_name = criterion.__class__.__name__
     print(f"\n--- Model Evaluation ---")
     print(f"Wholes Test {criterion_name}: {test_loss_wholes:.4e}")
     print(f"Halves Test {criterion_name}: {test_loss_halves:.4e}")
+
+
+
+
+
     with open(LOGS_PATH, "a+") as logs_file:
         logs_file.write(f"Wholes Test {criterion_name}: {test_loss_wholes:.4e}\n")
         logs_file.write(f"Halves Test {criterion_name}: {test_loss_halves:.4e}\n")
@@ -137,22 +159,66 @@ X_tensor = torch.tensor(normalized_x, dtype=torch.float32).to(device)
 n_samples = n_samples = len(X_tensor)
 
 
-weights_mask = np.repeat(weights_mask[:, np.newaxis, :], 3, axis=1)
-weights_tensor = torch.tensor(weights_mask, device=device)
+weights_mask_train = np.repeat(weights_mask_train[:, np.newaxis, :], 3, axis=1)
+weights_tensor_train = torch.tensor(weights_mask_train, device=device)
 
-def weighted_mse_loss(idx, pred, target):
-    eps = 1e-6
-    # print(idx.tolist())
-    # print("indices ", len(indices))
+def weighted_mse_loss(pred, target, idx):
+    eps = 1e-9
     sq_err_batch = (pred - target) / (target.abs() + eps)
-    # print("sq_err ", sq_err_batch.shape)
-    
+
     indexes = idx.detach().cpu().numpy() // seeds_per_energy
-    # batch_weights = torch.tensor(np.repeat(weights_mask[indices_tensor][:, np.newaxis, :], 3, axis=1), device=pred.device)
-    # print("weights_tensor", weights_tensor.shape)
-    weighted = (sq_err_batch**2) * weights_tensor[indexes]
+    weighted = (sq_err_batch**2) * weights_tensor_train[indexes]
+
     return weighted.mean()
 
+
+weights_mask_test = np.repeat(weights_mask_test[:, np.newaxis, :], 3, axis=1)
+weights_tensor_test = torch.tensor(weights_mask_test, device=device)
+
+def test_weighted_mse(model):
+    eps = 1e-9
+    model.eval()
+
+    normalized_data_dose_test_wholes, normalized_data_dose_test_halves = normalized_data_dose_test[::2], normalized_data_dose_test[1::2]
+    normalized_data_fluence_protons_test_wholes, normalized_data_fluence_protons_test_halves = normalized_data_fluence_protons_test[::2], normalized_data_fluence_protons_test[1::2]
+    normalized_data_dlet_protons_test_wholes, normalized_data_dlet_protons_test_halves = normalized_data_dlet_protons_test[::2], normalized_data_dlet_protons_test[1::2]
+    data_x_test_wholes, data_x_test_halves = data_x_test[::2], data_x_test[1::2]
+
+
+
+    Y_test_wholes = torch.stack(
+        [
+            torch.tensor(normalized_data_dose_test_wholes, dtype=torch.float32),
+            torch.tensor(normalized_data_fluence_protons_test_wholes, dtype=torch.float32),
+            torch.tensor(normalized_data_dlet_protons_test_wholes, dtype=torch.float32),
+        ],
+        dim=1,
+    ).to(device)
+
+    Y_test_halves = torch.stack(
+        [
+            torch.tensor(normalized_data_dose_test_halves, dtype=torch.float32),
+            torch.tensor(normalized_data_fluence_protons_test_halves, dtype=torch.float32),
+            torch.tensor(normalized_data_dlet_protons_test_halves, dtype=torch.float32),
+        ],
+        dim=1,
+    ).to(device)
+
+    X_test_wholes_tensor = torch.tensor(data_x_test_wholes, dtype=torch.float32).to(device)
+    X_test_halves_tensor = torch.tensor(data_x_test_halves, dtype=torch.float32).to(device)
+
+    weights_testor_test_wholes, weights_testor_test_halves = weights_tensor_test[::2], weights_tensor_test[1::2]
+    with torch.no_grad():
+        predictions_wholes = model(X_test_wholes_tensor)
+
+        sq_err_batch = (predictions_wholes - Y_test_wholes) / (Y_test_wholes.abs() + eps)
+        weighted_test_loss_wholes = (sq_err_batch**2) * weights_testor_test_wholes
+
+        predictions_halves = model(X_test_halves_tensor)
+        sq_err_batch = (predictions_halves - Y_test_halves) / (Y_test_halves.abs() + eps)
+        weighted_test_loss_halves = (sq_err_batch**2) * weights_testor_test_halves
+
+    return weighted_test_loss_wholes.mean(), weighted_test_loss_halves.mean(), 
 
 
 class Model(nn.Module):
@@ -187,12 +253,13 @@ class Model(nn.Module):
         return torch.stack([dose, fluence, let], dim=1)
 
 batch_size = 128
-total_epochs = 600
+total_epochs = 300
 
 model     = Model().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=20, factor=0.5)
-criterion = nn.MSELoss()
+# criterion = nn.MSELoss()
+# criterion = weighted_mse_loss
 
 model.train()
 start_training = time.time()
@@ -206,8 +273,8 @@ for epoch in range(total_epochs):
 
         optimizer.zero_grad()
         pred = model(x_batch)
-        # loss = weighted_mse_loss(idx, pred, y_batch)
-        loss = criterion(pred, y_batch)
+        loss = weighted_mse_loss(pred, y_batch, idx)
+        # loss = criterion(pred, y_batch, idx)
         loss.backward()
         optimizer.step()
         train_loss += loss.item()
@@ -227,7 +294,7 @@ for epoch in range(total_epochs):
                 f"Epoch {epoch:>4d} | Train loss: {train_loss:.8e}  | LR: {current_lr:.2e} | Time: {time.time()-start_training:.2f}\n"
             )
 
-final_test_loss_wholes, final_test_loss_halves = test_model(model, criterion, device)
+final_test_loss_wholes, final_test_loss_halves = test_weighted_mse(model)
 
 CHECKPOINTS_DIR_NAME = "checkpoints"+slurm_job_id
 def save_checkpoints():
@@ -241,7 +308,7 @@ def save_checkpoints():
         best_loss_file.write(f"{final_test_loss_wholes},{final_test_loss_halves}")
         # os.makedirs('./checkpoints', exist_ok=True)
     # with open(Path(HOME, CHECKPOINTS_DIR_NAME, "best_code"+str(losses_number)), "w") as best_code_file:
-    #     with open(Path(HOME,"tmp","train_model_initial_wieghted_loss.py"), "r") as current_code_file:
+    #     with open(Path(HOME,"TMP_DIR","train_model_initial_wieghted_loss.py"), "r") as current_code_file:
     #         best_code_file.write(current_code_file.read())
 
 alpha = 0.75
@@ -260,5 +327,5 @@ else:
         combined_best_loss = alpha * best_loss_wholes + (1 - alpha) * best_loss_halves
         combined_loss = alpha * final_test_loss_wholes + (1 - alpha) * final_test_loss_halves
         
-    if combined_loss < combined_best_loss:
+    if final_test_loss_wholes < best_loss_wholes or final_test_loss_halves < best_loss_halves:
         save_checkpoints()
